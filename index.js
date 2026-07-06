409d17c639fdae3efc964b78abe56031df49cab6
Maluki Victor <victormaluki0@gmail.com>
2026-07-06 09:31:58 +0300

    login tracker js trial one

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
 
+    if (
+      passwordAttempt === userInfo.password &&
+      attemptCount < 3
+    ) {
+      return "login successful";
+    }
+
+    if (attemptCount > 3) {
+      return "account locked due to too many failed login attempts";
+    }
+
+    return `attempt ${attemptCount}: login failed`;
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+};  
+
+
+
+
+
+};  
+
+
+
